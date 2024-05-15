7f72cf2a0c701ee2b72c4bd37b086bb6f1f8a02f
Ning Kang <nkang@agl.com.au>
2024-05-15 13:15:11 +1000

    traverse

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/ning-kang/webcrawler
 
 go 1.22.1
 
-require github.com/spf13/cobra v1.8.0
+require (
+	github.com/spf13/cobra v1.8.0
+	golang.org/x/net v0.25.0
+)
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
--- webcrawler/internal/scrape.go
+++ webcrawler/internal/scrape.go
@@ -1,7 +1,52 @@
 package internal
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"golang.org/x/net/html"
+)
 
 func ScrapeBooks(url string) {
 	fmt.Println("Scraping books from", url)
+	html, err := get(url)
+	if err != nil {
+		log.Fatal("Unable to scrape book:", err)
+	}
+	traverseHTML(html)
+}
+
+func get(url string) (*html.Node, error) {
+	client := http.Client{
+		Timeout: 30 * time.Second,
+	}
+
+	res, err := client.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, err
+	}
+
+	html, err := html.Parse(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	return html, nil
+}
+
+func traverseHTML(n *html.Node) {
+	if n.Type == html.ElementNode {
+		fmt.Println(n.Data)
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		traverseHTML(c)
+	}
 }
